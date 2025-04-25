740211c4066a97fa2bcf48983249d34b7e595510
submonitoring <submonitoring@outlook.com>
2025-04-25 10:41:54 +0700

    add InitialSeeder seeder class

---
+++ database/seeders/InitialSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class InitialSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('panel_roles')->insert([
+            'id' => 1,
+            'unique' => Str::ulid(),
+            'record_title' => 'submonitoring',
+            'panel_role' => 'submonitoring',
+            'is_active' => 1,
+        ]);
+
+        DB::table('users')->insert([
+            'id' => 1,
+            'unique' => Str::ulid(),
+            'record_title' => 'Submonitoring',
+            'name' => 'submonitoring',
+            'user_name' => 'submonitoring',
+            'panel_role_id' => 1,
+            'email' => 'submonitoring@outlook.com',
+            'password' => Hash::make('s3ng0n46'),
+        ]);
+    }
+}
