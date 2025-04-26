c445d79a20f7915956ad88c2cf5aada8293d3b4e
submonitoring <submonitoring@outlook.com>
2025-04-26 16:07:38 +0700

    fix remove delete button on EditBusinessPartner and ViewBusinessPartner on jhpadmin panel

--- app/Filament/Jhpadmin/Resources/BusinessPartnerResource/Pages/EditBusinessPartner.php
+++ app/Filament/Jhpadmin/Resources/BusinessPartnerResource/Pages/EditBusinessPartner.php
@@ -4,12 +4,81 @@ namespace App\Filament\Jhpadmin\Resources\BusinessPartnerResource\Pages;
 
 use App\editpage;
 use App\Filament\Jhpadmin\Resources\BusinessPartnerResource;
+use App\Filament\Resources\Pages\Concerns\CanPaginateViewRecord;
+use App\Filament\Submonitoring\Resources\Actions\NextAction;
+use App\Filament\Submonitoring\Resources\Actions\PreviousAction;
+use App\Models\BusinessPartnerCompany;
+use App\Models\BusinessPartnerCustomer;
+use App\Models\BusinessPartnerVendor;
 use Filament\Actions;
+use Filament\Actions\Action;
+use Filament\Actions\ViewAction;
 use Filament\Resources\Pages\EditRecord;
+use Howdu\FilamentRecordSwitcher\Filament\Concerns\HasRecordSwitcher;
+use Kenepa\ResourceLock\Resources\Pages\Concerns\UsesResourceLock;
 
 class EditBusinessPartner extends EditRecord
 {
     protected static string $resource = BusinessPartnerResource::class;
 
-    use editpage;
+    protected function afterSave(): void
+    {
+
+        $record = $this->getRecord();
+
+        // dd($record, in_array(1, $record->bp_role_id));
+        $cekbpcomp = BusinessPartnerCompany::where('business_partner_id', $record->id)->pluck('id')->toArray();
+        $cekbpcust = BusinessPartnerCustomer::where('business_partner_id', $record->id)->pluck('id')->toArray();
+        $cekbpvend = BusinessPartnerVendor::where('business_partner_id', $record->id)->pluck('id')->toArray();
+
+        if (!in_array(1, $record->bp_role_id) ==  true) {
+
+            BusinessPartnerCustomer::whereIn('id', $cekbpcust)->update(['is_active' => 0]);
+        }
+
+        if (!in_array(2, $record->bp_role_id) ==  true) {
+
+            BusinessPartnerVendor::whereIn('id', $cekbpvend)->update(['is_active' => 0]);
+        }
+
+        if (in_array(1, $record->bp_role_id) ==  true) {
+
+            BusinessPartnerCustomer::whereIn('id', $cekbpcust)->update(['is_active' => 1]);
+        }
+
+        if (in_array(2, $record->bp_role_id) ==  true) {
+
+            BusinessPartnerVendor::whereIn('id', $cekbpvend)->update(['is_active' => 1]);
+        }
+
+        if ($record->is_active == false) {
+
+            BusinessPartnerCompany::whereIn('id', $cekbpcomp)->update(['is_active' => 0]);
+
+            BusinessPartnerCustomer::whereIn('id', $cekbpcust)->update(['is_active' => 0]);
+
+            BusinessPartnerVendor::whereIn('id', $cekbpvend)->update(['is_active' => 0]);
+        }
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            PreviousAction::make(),
+            NextAction::make(),
+            ViewAction::make(),
+            // DeleteAction::make(),
+            Action::make('Back to List')
+                ->url($this->getResource()::getUrl('index')),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    use UsesResourceLock;
+    use HasRecordSwitcher;
+    use CanPaginateViewRecord;
 }
