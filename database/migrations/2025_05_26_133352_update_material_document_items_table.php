ff5bad41151e70000e79be4875353d137a1db193
submonitoring <submonitoring@outlook.com>
2025-05-27 11:40:09 +0700

    -comment migratein

--- database/migrations/2025_05_26_133352_update_material_document_items_table.php
+++ database/migrations/2025_05_26_133352_update_material_document_items_table.php
@@ -5,17 +5,17 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('material_document_items', function (Blueprint $table) {
-            $table->foreignId('to_plant_id')->nullable()
-                ->constrained()
-                ->cascadeOnUpdate()
-                ->nullOnDelete();
+    // /**
+    //  * Run the migrations.
+    //  */
+    // public function up(): void
+    // {
+    //     Schema::table('material_document_items', function (Blueprint $table) {
+    //         $table->foreignId('to_plant_id')->nullable()
+    //             ->constrained()
+    //             ->cascadeOnUpdate()
+    //             ->nullOnDelete();
 
-        });
-    }
+    //     });
+    // }
 };
